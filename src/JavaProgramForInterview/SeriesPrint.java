34df2e663bd7abf1513ae8eca73ce26d6097b57a
Anup Sahu6 <anupsahu16@gmail.com>
2022-01-30 18:45:06 +0530

    Java Code for practice

---
+++ src/JavaProgramForInterview/SeriesPrint.java
@@ -0,0 +1,15 @@
+package JavaProgramForInterview;
+
+public class SeriesPrint {
+
+	public static void main(String[] args) {
+		for(int i = 0 ; i<=9;i++) {
+			for(int j = 0 ;j<=9;j++) {
+				System.out.print(i+""+j+" ");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
